862b7aba905fdb3f793de4dd1bf62dea43fdcd67
arleycn <arley@arley.cn>
2024-03-12 20:39:38 +0800

    fix

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">时光如梭，转瞬即逝。昨日稚嫩，今朝成熟；花开花落，岁月无声。朝霞与夕阳，一刹那辉煌，留下光阴的足迹，悄然间，已是人海茫茫。</div>
 </template>
 
 <style lang="scss" scoped>
